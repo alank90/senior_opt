4839fc56e42343d683f715df66394a66c48efc8b
Alan K <akillian@outlook.com>
2024-01-12 14:32:05 -0500

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Senior Options</h1>
 
-  <button>Get Rows</button>
+  <button @click="getRows">Get Rows</button>
 </template>
 
 <script setup>
   // ========= Vars ======================= //
 
+  async function getRows() {
+    const ssRows = await fetch(
+      "https://script.google.com/macros/s/AKfycbxAyycSrzihox8RpI2C_SNNnYyjrg6MTe-1nbvhvCZwJUyLPjObwo5FC4c9j44zOm0WPQ/exec"
+    );
+    console.log(ssRows);
+  }
 </script>
