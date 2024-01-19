c2b1fe8375596f9a13fb05348482b4fe69d12640
Alan K <akillian@outlook.com>
2024-01-19 14:25:05 -0500

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,26 @@
 <template>
   <h1>Senior Options</h1>
 
-  <button @click="getSSValues">Get Data</button>
+  <button @click="buttonClicked = !buttonClicked">Get Data</button>
 
-  <div>{{ ssData }}</div>
+  <div v-if="ssData && buttonClicked">{{ ssData.values[3][2] }}</div>
 </template>
 
 <script setup>
+  import { ref } from "vue";
   import { useFetch } from "../modules/useFetch.js";
 
   // ========= Variable Declarations ======================= //
   const API_KEY = import.meta.env.VITE_API_SS_KEY;
   const ssID = "12NuSzSyIIoVf5oQv2YyjLyGKYe4Pyyme0oPPj7G2pK0";
   const ssURL = `https://sheets.googleapis.com/v4/spreadsheets/${ssID}/values/students!A2:D10?key=${API_KEY}`;
+  let buttonClicked = ref(false);
 
-  // Fetch the SS
+  // Fetch the SS data
   // eslint-disable-next-line no-unused-vars
-
   const { data: ssData, loadingState, error } = useFetch(ssURL);
+
+  
 </script>
 
 <style scoped>
